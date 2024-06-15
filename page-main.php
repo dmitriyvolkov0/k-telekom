20fc882fe3f588b89c0520a3419707b441c1c796
Дмитрий <dmitriyvolkov002@gmail.com>
2024-06-15 12:31:52 +0300

    Сверстана шапка и баннер

--- page-main.php
+++ page-main.php
@@ -3,13 +3,19 @@
 ?>
 
 <section class="banner">
-	<div class="container">
-		<h1>Экономия нервов и денег — интернет К-Телеком</h1>
-		<ul>
-			<li>5 тарифов для многоквартирных и 4 для частных домов</li>
-			<li>безлимит подключенных устройств</li>
-			<li>комфортная поддержка 24/7</li>
-		</ul>
+	<div class="banner__container container">
+		<div class="banner__start">
+			<h1 class="banner__title">Экономия нервов и денег — интернет К-Телеком</h1>
+			<ul class="banner__list">
+				<li>5 тарифов для многоквартирных и 4 для частных домов</li>
+				<li>безлимит подключенных устройств</li>
+				<li>комфортная поддержка 24/7</li>
+			</ul>
+			<a href="#" class="banner__but but-fill">Подробнее</a>
+		</div>
+		<div class="banner__end">
+			<img class="banner__img" src="<?php echo get_template_directory_uri() . './assets/img/computer.png'?>" alt="Экономия нервов и денег — интернет К-Телеком">
+		</div>
 	</div>
 </section>
 
